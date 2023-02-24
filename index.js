940e0891f466cc259a5d87d0f70c0374df6d1036
Ahsan <fa19-bse-059@cuiatk.edu.pk>
2023-02-24 22:44:03 +0500

    Second Commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { port } = require("./src/config/Serverconfig");
+const body_parser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const StartServer = async () => {
   // create Express Object
   const app = express();
+
+  app.use(body_parser.json());
+  app.use(body_parser.urlencoded({ extended: true }));
 
   // start the server
   app.listen(port, () => {
