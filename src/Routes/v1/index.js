dfd6b26e5259d1fa72a23ddea9648f6a39f6e054
Ahsan <fa19-bse-059@cuiatk.edu.pk>
2023-03-02 15:53:16 +0500

    added Flight create request middleware

--- src/Routes/v1/index.js
+++ src/Routes/v1/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cityController = require("../../controllers/city_controller");
 const flightController = require("../../controllers/flight-Controller");
 const AirplaneController = require("../../controllers/airport-controller");
+const { FlightMiddlewares } = require("../../middlewares/index");
 const router = express.Router();
 
 router.post("/city", cityController.create);
@@ -11,7 +12,11 @@ router.patch("/city/:id", cityController.update);
 router.get("/city", cityController.getAllCity);
 router.get("/flights", flightController.getAllFlight);
 
-router.post("/flights", flightController.create);
+router.post(
+  "/flights",
+  FlightMiddlewares.validateCreateFlight,
+  flightController.create
+);
 router.post("/airports", AirplaneController.create);
 
 module.exports = router;
